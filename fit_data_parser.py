2d51d63a93361d6711976805266c684401a44c68
Mitchell Devlin <mitchell.r.devlin@gmail.com>
2021-02-27 15:04:35 -0500

    Fixed the xml parsing of the google fit tracking data, and added a print statement showing fitness metrics for the given data dir

--- fit_data_parser.py
+++ fit_data_parser.py
@@ -6,23 +6,34 @@ import os
 import ActivityData
 import xml.etree.ElementTree as ET
 
+DISTANCE_IDX=1
+TIME_S_IDX=2
+CALORIES_IDX=3
+
 def main():
     parser = argparse.ArgumentParser(description='Parse google fit data')
     parser.add_argument('data_dir', help='Pass in the google fit data directory')
 
     args = parser.parse_args()
-    activityData = parseRuns(args.data_dir)
+    activityData = parseActivities(args.data_dir)
     
 def parseActivities(fitDataDir):
+    totalDistanceM = 0.0
+    totalTimeS = 0.0
+    totalCalories = 0.0
+
     for filename in os.listdir(fitDataDir):
         if 'Running' or 'Treadmill' in filename:
-            xmlTree = ET.parse(os.path.join(fitDataDir, filename))
+            xmlTree = ET.parse(os.path.join(fitDataDir, filename)).getroot()
             activityList = []
-            for activity in xmlTree.iter('Lap'):
-                distance_m = activity['DistanceMeters'].text
-                time_s = activity['TotalTimeSeconds'].text
-                calories = activity['Calories'].text
-        
+            for activity in xmlTree.iter('{http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2}Lap'):
+                totalDistanceM += float(activity[DISTANCE_IDX].text)
+                totalTimeS += float(activity[TIME_S_IDX].text)
+                totalCalories += float(activity[CALORIES_IDX].text)
+    
+    print("Total distance (meters):", totalDistanceM)
+    print("Total duration (seconds):", totalTimeS)
+    print("Total calories: (kcal):", totalCalories)
 
 if __name__ == '__main__':
     main() 
